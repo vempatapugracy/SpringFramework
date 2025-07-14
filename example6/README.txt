5a14a4cb9972ef731884359bfd6a1ca970eecdbc
vempatapugracy <gracyvempatapu2000@gmail.com>
2025-07-14 19:46:54 +0530

    @PostConstruct annotation usage

---
+++ example6/README.txt
@@ -0,0 +1,7 @@
+@PostConstruct
+
+With Stereotype annotations, there is no control on object instance, if we want to perform some operations automatically after object creation we can use @PostConstruct annotation
+It's in Jakarta EE
+To use this annotation, need to add jakarta-annotation dependency in pom.xml
+
+
---
+++ example6/src/main/java/com/gracy/beans/Vehicle.java
@@ -0,0 +1,26 @@
+package com.gracy.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Printing Hello from Component Vehicle Bean!!");
+    }
+
+    @PostConstruct
+    public void initialize(){
+        this.name="Gracy";
+    }
+}
